a893a6640734e225334c3776d0fa1cf2167bf041
vardanyan-karen <armen1979vardanyan@gmail.com>
2024-10-14 23:13:56 +0400

    disable "enableEdgeToEdge"

--- app/src/main/java/symply/homework/travelulu/MainActivity.kt
+++ app/src/main/java/symply/homework/travelulu/MainActivity.kt
@@ -3,13 +3,12 @@ package symply.homework.travelulu
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import symply.homework.travelulu.commonpresentation.ui.theme.BasicJetpackComposeProjectTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+//        enableEdgeToEdge()
         setContent {
             BasicJetpackComposeProjectTheme {
                 MainScreen()
